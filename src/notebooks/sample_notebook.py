6bf3113e25165079b9ec575c3bebf67c0d22fa23
Sairam <rasairam@deloitte.com>
2026-01-29 10:48:20 +0530

    Updated datbricks.yml

--- src/notebooks/sample_notebook.py
+++ src/notebooks/sample_notebook.py
@@ -1,15 +1,19 @@
+# Databricks notebook source
 from pyspark.sql import SparkSession
- 
+
 spark = SparkSession.builder.getOrCreate()
- 
+
+# COMMAND ----------
 data = [
     (1, "Ram"),
     (2, "Databricks"),
     (3, "Jenkins")
 ]
- 
+
 df = spark.createDataFrame(data, ["id", "name"])
- 
+
+# COMMAND ----------
 df.show()
- 
+
+# COMMAND ----------
 df.write.mode("overwrite").saveAsTable("default.sample_table")
